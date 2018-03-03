0dbec753698ec7c4656b8cc833afe8bdefc7bf7d
Aleksei Podgaev <podgaev@gmail.com>
2018-03-03 17:43:34 +0100

    proper library building with webpack - peer dependencies removed from bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,48 @@
 module.exports = {
-    entry: "./src/index.ts",
-    output: {
-        filename: "index.js",
-        path: __dirname + "/lib"
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: __dirname + "/lib",
+    library: 'i18nextSaga',
+    libraryTarget:'umd'
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.ts$/, loader: "awesome-typescript-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+
+  externals: {
+    i18next: {
+      commonjs: 'i18next',
+      commonjs2: 'i18next',
+      amd: 'i18next',
+      root: 'i18next'
     },
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".js", ".json"]
+    "redux-saga/effects": {
+      commonjs: 'redux-saga/effects',
+      commonjs2: 'redux-saga/effects',
+      amd: 'redux-saga/effects',
+      root: 'ReduxSaga.effects'
     },
-
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.ts$/, loader: "awesome-typescript-loader" },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
+    redux: {
+      commonjs: 'redux',
+      commonjs2: 'redux',
+      amd: 'redux',
+      root: 'redux'
     },
-
+  }
 };
