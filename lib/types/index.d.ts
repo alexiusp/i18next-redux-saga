08630402f9de8b1db17394bc22f226a049635abe
Aleksei Podgaev <aleksei@neuronation.de>
2018-06-26 15:31:13 +0200

    trying to fix missing babel polyfill issue

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,3 +1,4 @@
+import "regenerator-runtime/runtime";
 export * from './actions';
 import _i18nextSaga from './saga';
 export declare const i18nextSaga: typeof _i18nextSaga;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "regenerator-runtime/runtime";
 
 export * from './actions';
 
