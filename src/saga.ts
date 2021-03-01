f132d1d5fd91a42234258db3e60c66a9f0bcf943
Aleksei Podgaev <podgaev@gmail.com>
2021-03-01 01:56:54 +0100

    imports and typing improved

--- src/saga.ts
+++ src/saga.ts
@@ -1,93 +1,71 @@
 import I18Next from 'i18next';
 import { SagaIterator } from 'redux-saga';
 import { all, call, put, takeEvery } from 'redux-saga/effects';
-import {
-  I18NEXT_CHANGE_LANGUAGE,
-  I18NEXT_CREATE_INSTANCE,
-  I18NEXT_INIT,
-  I18NEXT_LOAD_LANGUAGES,
-  I18NEXT_LOAD_NAMESPACES,
-  I18NEXT_USE,
-  ICreateInstanceAction,
-} from './actionTypes';
-import {
-  IChangeLanguageAction,
-  IInitAction,
-  ILoadLanguagesAction,
-  ILoadNamespacesAction,
-  IUseAction,
-} from './actionTypes';
-import {
-  i18nextChangeLanguageReady,
-  i18nextCreateInstanceReady,
-  i18nextError,
-  i18nextLoadLanguagesReady,
-  i18nextLoadNamespacesReady,
-  i18nextReady,
-} from './actions';
+import * as ActionTypes from './actionTypes';
+import * as Actions from './actions';
 
-function* i18nextUseSaga(action: IUseAction): SagaIterator {
-  yield call([I18Next, 'use'], action.module);
+function* i18nextUseSaga({ module }: ActionTypes.IUseAction): SagaIterator {
+  yield call([I18Next, 'use'], module);
 }
 
-function* i18nextInitSaga(action: IInitAction): SagaIterator {
+function* i18nextInitSaga({ options }: ActionTypes.IInitAction): SagaIterator {
   try {
-    yield call([I18Next, 'init'], action.options);
-    yield put(i18nextReady(I18Next));
+    yield call([I18Next, 'init'], options);
+    yield put(Actions.i18nextReady(I18Next));
   } catch (error) {
     console.error(error);
-    yield put(i18nextError(error));
+    yield put(Actions.i18nextError(error));
   }
 }
 
-function* i18nextChangeLanguageSaga(action: IChangeLanguageAction): SagaIterator {
+function* i18nextChangeLanguageSaga({ language }: ActionTypes.IChangeLanguageAction): SagaIterator {
   try {
-    yield call([I18Next, 'changeLanguage'], action.language);
-    yield put(i18nextChangeLanguageReady());
+    yield call([I18Next, 'changeLanguage'], language);
+    yield put(Actions.i18nextChangeLanguageReady());
   } catch (error) {
     console.error(error);
-    yield put(i18nextError(error));
+    yield put(Actions.i18nextError(error));
   }
 }
 
-function* i18nextLoadNamespacesSaga(action: ILoadNamespacesAction): SagaIterator {
+function* i18nextLoadNamespacesSaga({ ns }: ActionTypes.ILoadNamespacesAction): SagaIterator {
   try {
-    yield call([I18Next, 'loadNamespaces'], action.ns);
-    yield put(i18nextLoadNamespacesReady());
+    yield call([I18Next, 'loadNamespaces'], ns);
+    yield put(Actions.i18nextLoadNamespacesReady());
   } catch (error) {
     console.error(error);
-    yield put(i18nextError(error));
+    yield put(Actions.i18nextError(error));
   }
 }
 
-function* i18nextLoadLanguagesSaga(action: ILoadLanguagesAction): SagaIterator {
+function* i18nextLoadLanguagesSaga({ languages }: ActionTypes.ILoadLanguagesAction): SagaIterator {
   try {
-    yield call([I18Next, 'loadLanguages'], action.languages);
-    yield put(i18nextLoadLanguagesReady());
+    yield call([I18Next, 'loadLanguages'], languages);
+    yield put(Actions.i18nextLoadLanguagesReady());
   } catch (error) {
     console.error(error);
-    yield put(i18nextError(error));
+    yield put(Actions.i18nextError(error));
   }
 }
 
-function* i18nextCreateInstance(action: ICreateInstanceAction): SagaIterator {
+function* i18nextCreateInstance({ options }: ActionTypes.ICreateInstanceAction): SagaIterator {
   try {
     const newInstance = I18Next.createInstance();
-    yield call([newInstance, 'init'], action.options);
-    yield put(i18nextCreateInstanceReady(newInstance));
+    yield call([newInstance, 'init'], options);
+    yield put(Actions.i18nextCreateInstanceReady(newInstance));
   } catch (error) {
     console.error(error);
-    yield put(i18nextError(error));
+    yield put(Actions.i18nextError(error));
   }
 }
 
 export default function* i18nextSaga(): SagaIterator {
   return yield all([
-    yield takeEvery(I18NEXT_INIT, i18nextInitSaga),
-    yield takeEvery(I18NEXT_USE, i18nextUseSaga),
-    yield takeEvery(I18NEXT_CHANGE_LANGUAGE, i18nextChangeLanguageSaga),
-    yield takeEvery(I18NEXT_LOAD_NAMESPACES, i18nextLoadNamespacesSaga),
-    yield takeEvery(I18NEXT_LOAD_LANGUAGES, i18nextLoadLanguagesSaga),
-    yield takeEvery(I18NEXT_CREATE_INSTANCE, i18nextCreateInstance),
+    yield takeEvery(ActionTypes.I18NEXT_INIT, i18nextInitSaga),
+    yield takeEvery(ActionTypes.I18NEXT_USE, i18nextUseSaga),
+    yield takeEvery(ActionTypes.I18NEXT_CHANGE_LANGUAGE, i18nextChangeLanguageSaga),
+    yield takeEvery(ActionTypes.I18NEXT_LOAD_NAMESPACES, i18nextLoadNamespacesSaga),
+    yield takeEvery(ActionTypes.I18NEXT_LOAD_LANGUAGES, i18nextLoadLanguagesSaga),
+    yield takeEvery(ActionTypes.I18NEXT_CREATE_INSTANCE, i18nextCreateInstance),
   ]);
 }
